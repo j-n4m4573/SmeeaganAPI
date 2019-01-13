2c420349141fde9348dccb9651a17cc7928121c2
j-n4m4573 <jg_220@yahoo.com>
2019-01-13 12:25:15 -0800

    add Product model

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost:27017/SmeeaganAPI');
+
+var router = express.Router();
+
+app.use('/api', router);
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!')
-})
+// app.listen(3000, () => {
+//   console.log('App listening on port 3000!')
+// })
 
-mongoose.connect('mongodb://localhost/SmeeaganAPI');
+
+
+
+
+app.listen(port);
+console.log('Server listening on port ' + port);
---
+++ models/product.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProductSchema = new Schema ({
+    name : String,
+    id: String,
+    photo: String
+})
+
+module.exports = mongoose.model('Product', ProductSchema) 
