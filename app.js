76398504c68f973eae9cf5eefe2486f13e186671
j-n4m4573 <jg_220@yahoo.com>
2019-01-13 13:04:37 -0800

    add product routes

--- app.js
+++ app.js
@@ -2,25 +2,78 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var Product = require('./models/product')
 
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+
 var port = process.env.PORT || 3000;
 
 mongoose.connect('mongodb://localhost:27017/SmeeaganAPI');
 
 var router = express.Router();
-
+// Routes fixed with api prefix
 app.use('/api', router);
 
+// Middleware
+// middleware to use for all requests
+router.use(function(req, res, next) {
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+    console.log("Hello, processing going down... aight.. hang tight")
+    next()
 })
+
+// TEST ROUTE
+router.get('/', (req, res) => {
+    res.json({message: 'Welcome to our API!'})
+})
+
+router.route('/prouducts')
+    .post(function(req, res) {
+        var product = new Product()
+        product.name = req.body.name;
+        product.id = req.body.id;
+        product.photo = req.body.photo;
+
+        product.save(function(err) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({message: "Product was successfully created"});
+        });
+    })
+
+    .get(function(req, res) {
+        Product.find(function(err, products) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(products);
+        });
+    });
+
+router.route('/product/:product_id')
+    .get(function(req, res) {
+        Product.findById(req.params.product_id, function(err, product) {
+            if (err) {
+                res.send(err)
+            }
+            res.json(product)
+        });
+    });
+
+router.route('/product/:photo')
+    .get(function(req, res) {
+        Product.find({photo:req.params.photo}, function(err, product) {
+            if (err) {
+                res.send(err)
+            }
+            res.json(product)
+        });
+    }); 
 
 // app.listen(3000, () => {
 //   console.log('App listening on port 3000!')
