bcadf2aa2000aed5ada12f3fa9b47b252769ed40
j-n4m4573 <jg_220@yahoo.com>
2019-01-13 11:40:05 -0800

    added body-parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -8,3 +9,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
+
+mongoose.connect('mongodb://localhost/SmeeaganAPI');
